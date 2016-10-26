40d230fd3125598a1555d11a979e9dfe0ac665f6
Семен Никулин <snikulin@spaceotechnologies.ru>
2016-10-26 18:03:58 +0700

    Initial Commit

---
+++ AlgoritmA/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AlgoritmA
+//
+//  Created by Семен Никулин on 26.10.16.
+//  Copyright © 2016 niksemv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
